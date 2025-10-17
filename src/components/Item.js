386277f6738aba39c8abf1ee610fe59abe1b3431
Andrew Sigei <andrew.sigei@student.moringaschool.com>
2025-10-18 00:32:16 +0300

    ginal commit

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  let [inCart, setInCart] = useState(false);
+  function handleClick() {
+    setInCart((inCart) => !inCart);
+  }
+
+  const liClass= inCart ? "in-cart" : "";
+  const buttonText = inCart ? "Remove From Cart" : "Add to Cart";
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>
+        {buttonText}
+      </button>
     </li>
   );
 }
